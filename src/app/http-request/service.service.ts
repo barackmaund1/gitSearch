d93de4dc081c36e0c198c6b048d0ebfb1e4b77d3
barackmaund1 <pmaundu49@gmail.co>
2020-04-09 22:51:49 +0300

    added methods in the service and api

--- src/app/http-request/service.service.ts
+++ src/app/http-request/service.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { Repo } from './../repo-class/repo';
 import { User } from './../user-class/user';
 import { Injectable } from '@angular/core';
@@ -27,8 +28,38 @@ SearchUser(searchName:string){
   }
 
   return new Promise((resolve,reject)=>{
-    
+    this.http.get<Response>('https://api.github.com/users/'+searchName+'?access_token='+environment.apiKey).toPromise().then( 
+      (result)=>{
+        this.getUser=result;
+        console.log(this.getUser);
+        resolve()
+      },
+      (error)=>{
+        console.log(error);
+        reject();
+      }
+    )
   })
 }
-
+getReopos(searchName){
+ interface Repo{
+  name:string,
+  repo_url:string,
+  description:string,
+  language:string,
+  updated:Date
+ }
+ return new Promise((resolve,reject)=>{
+   this.http.get<Repo>('https:api.github.com/users/'+searchName+'/repo?order+created&sort=asc?access_token='+environment.apiKey).toPromise().then(
+     (results)=>{
+       this.getRepos=results;
+       resolve();
+     },
+     (error)=>{
+       console.log(error);
+       reject()
+     }
+   )
+ })
+}
 } 
